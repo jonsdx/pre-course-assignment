0e1eaab01d8ff423433c3a23820f1d898d01f7fa
jonsdx <62048791+jonsdx@users.noreply.github.com>
2023-09-18 23:43:34 +0800

    yyyy

--- workshop01/backend/server.js
+++ workshop01/backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(cors({ origin: "*" }));
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
 });
